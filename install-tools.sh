11bbfddb551cf463a9b219d3cf00b114bbb307d6
kiwenlau <kiwenlau@126.com>
2015-11-23 21:14:14 +0900

    install failed

--- install-tools.sh
+++ install-tools.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-#Install tophat2/ 2.0.9, bowtie2 2.1.0, samtools 0.1.18 and cufflinks 2.2.1
+# install samtools, bowtie2, tophat2 and cufflinks 
+
+apt-get update
+apt-get install unzip
 
 # download 
-wget http://depot.galaxyproject.org/package/linux/x86_64/samtools/samtools-0.1.18-linux-x86_64.tgz samtools-0.1.18-linux-x86_64.tgz 
-wget http://depot.galaxyproject.org/package/linux/x86_64/bowtie2/bowtie2-2.1.0-linux-x86_64.tgz bowtie2-2.1.0-linux-x86_64.tgz
-wget http://depot.galaxyproject.org/package/linux/x86_64/tophat/tophat-2.0.9.Linux_x86_64.tar.gz tophat-2.0.9.Linux_x86_64.tar.gz
+wget -O samtools-1.2.tar.bz2 http://sourceforge.net/projects/samtools/files/samtools/1.2/samtools-1.2.tar.bz2/download 
+wget -O bowtie2-2.2.6.zip http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.6/bowtie2-2.2.6-linux-x86_64.zip/download 
+wget https://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.0.Linux_x86_64.tar.gz
 wget http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.2.1.Linux_x86_64.tar.gz cufflinks-2.2.1.Linux_x86_64.tar.gz
 
 # extract
-tar -zxf samtools-0.1.18-linux-x86_64.tgz
-tar -zxf bowtie2-2.1.0-linux-x86_64.tgz
-tar -zxf tophat-2.0.9.Linux_x86_64.tar.gz
+tar -jxf samtools-1.2.tar.bz2
+unzip bowtie2-2.2.6.zip
+tar -zxf tophat-2.1.0.Linux_x86_64.tar.gz
 tar -zxf cufflinks-2.2.1.Linux_x86_64.tar.gz
 
 
-cp -r samtools-0.1.18/bin/samtools /usr/local/bin
-cp -r bowtie2-2.1.0 /usr/local/bin/bowtie2
-cp -r tophat-2.0.9.Linux_x86_64 /usr/local/bin/tophat2 
+cp -r samtools-1.2 /usr/local/bin/samtools
+cp -r bowtie2-2.2.6 /usr/local/bin/bowtie2
+cp -r tophat-2.1.0.Linux_x86_64 /usr/local/bin/tophat2 
+cp -r cufflinks-2.2.1.Linux_x86_64 /usr/local/bin/cufflinks
 
 
 rm -rf tophat* bowtie2* samtools*
 
 # set envrionment variable PATH
-export PATH="/usr/local/bin/samtools/bin:/usr/local/bin/bowtie2:/usr/local/bin/tophat2:/usr/local/bin/cufflinks:$PATH" 
+PATH="/usr/local/bin/samtools/bin:/usr/local/bin/bowtie2:/usr/local/bin/tophat2:/usr/local/bin/cufflinks:$PATH" 
